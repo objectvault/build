56c417f05237a45aa91fd1aa8ff3ac95ff91390e
Paulo Ferreira <pf@sourcenotes.org>
2022-09-23 13:59:50 +0100

    Cleaned up settings

--- sources.example/settings.sh
+++ sources.example/settings.sh
@@ -13,28 +13,30 @@ fi
 ## Base Script Directory
 export BASEDIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
-## WORKING MODE [DEFAULT: debug]
-MODE=${MODE:-"debug"}
-
 ## GITHUB ObjectVault Project BASE URL
 GITHUB_OV_URL="https://github.com/objectvault"
 
-## IMAGES
+## DOCKER Settings
+
+# IMAGES
 RABBITMQ="rabbitmq:management-alpine"
 MARIADB="bitnami/mariadb:latest"
 
-## BUILD Directory
+# BUILD Directory
 BUILDDIR="${BASEDIR}/builds"
 
-## CONTAINERS Directory for Container Data and Configuration
+# CONTAINERS Directory for Container Data and Configuration
 CONTAINERDIR="${BASEDIR}/containers"
 
-## CONTAINERS Source Directory for Container Configuration
+# CONTAINERS Source Directory for Container Configuration
 SOURCEDIR="${BASEDIR}/sources"
 
-## NETWORKS
-NETWORKS="net-ov-storage"
+# NETWORKS
+# Backend Network BUS
+NET_BACKEND="net-ov-backend"
 
-## DATABASE Related settings
+## MariaDB Container
+# DIR for Dumps
 DB_DUMPSDIR="${BASEDIR}/dumps"
+# DIR for DB Schema
 DB_SCHEMADIR="${BASEDIR}/init"
