26c7a46f230e300ddb138e30845eb6d0541a4705
Paulo Ferreira <pf@sourcenotes.org>
2022-09-28 15:38:56 +0100

    FIX: Fixed utility functions.

--- lib/utility.sh
+++ lib/utility.sh
@@ -13,17 +13,27 @@
 
 # Check if a Value Exists in List
 in_list() {
-  local key=$1  # Value to Search for
-  local list=$2 # List of Values
+  ## Example usage
+  # inlist "key" "${ARRAY[@]}"
+  # inlist "debug" "$MODES[@]"
+  ## RETURNS
+  # 0  - No match
+  # >0 - Index of Match (Not Zero Based)
+
+  local key=$1      # Value to Search for
+  local list=${@:2} # List of Values
+
+  #echo "PARAM 1 [$1]"
+  #echo "PARAM 2 [${list[@]}]"
 
   # Initialize Counter
   local index=0 # Initialize Counter
 
   # Loop through List
-  for k in $list; do
+  for k in ${list[@]}; do
     # Does KEY match?
     if [ "$k" = "$key" ]; then # YES: Return Index
-      return $index
+      return $((index+1))
     fi
 
     # Increment Counter
@@ -35,13 +45,28 @@ in_list() {
 }
 
 in_list_or_default() {
-  local default=$3
+  ## Example usage
+  # in_list_or_default "key" "${ARRAY[@]}" "default"
+  # in_list_or_default $3 "${MODES[@]}" "debug"
+  ## RETURNS
+  # default - No match in List
+  # match   - Matched Value in List
+
+  ## Function Arguments
+  local args=("$@")
+  local largs=${#args[@]}
+
+  # Params to pass o in_list call
+  local params=(${args[@]:0:(($largs - 1))})
+  # Default Valut
+  local default=${args[*]: -1}
 
   # Is Item in List?
-  local index=$(in_list $1 $2)
-  if [[ $MODEI == 0 ]]; then # NO: Return Default
+  in_list "${params[@]}"
+  local index=$?
+  if [[ $index == 0 ]]; then # NO: Return Default
     echo "${default}"
+  else # YES: Return Value
+    echo ${params[@]:$index:1}
   fi
-  # ELSE: Return Value
-  echo $1
 }
