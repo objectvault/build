87820cf8b7711cac05cfec33ea8a1e87dcd2aec2
Paulo Ferreira <pf@sourcenotes.org>
2022-09-30 14:55:01 +0100

    Added command handlers for API and FE Services

--- lib/api.sh
+++ lib/api.sh
@@ -11,47 +11,36 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-## Build Docker Image for API Server
-build_api() {
-  REPO="api-services"
-  VERSION="v0.0.1"
-  IMAGE="ov-api-server"
+# API Server Properties
+API_REPO="api-services"
+API_IMAGE="ov-api-server"
+API_VERSION="v0.0.1"
 
-  # Stage Image from GITHUB
-  github_clone_release "${REPO}" "${VERSION}"
-
-  # Build Docker Image
-  build_docker_image "${REPO}" "local/${IMAGE}:${VERSION}"
-}
-
-## Start Backend API Server
-start_api() {
-  # PARAM $1 - Container Name
-  CONTAINER=$1
-
-  # Docker Image
-  IMAGE="ov-api-server"
-  VERSION="v0.0.1"
-  DOCKER_IMAGE="local/${IMAGE}:${VERSION}"
+## HELPERS ##
+__api_start_server() {
+  # INPUTS
+  local image=$1     # Docker Image Name
+  local container=$2 # Container Name
+  local mode=$3      # Mode
 
   # Is Container Running?
-  status container "$CONTAINER"
+  status container "$container"
   if [[ $? == 0 ]]; then # NO
     ## Start Mongo
-    echo "Running container '$CONTAINER'"
+    echo "Running container '$container'"
 
     # Custom Configuration File
-    CONF="${CONTAINERDIR}/api/server.${MODE}.json"
+    local conf="${CONTAINERDIR}/api/server.${mode}.json"
 
     # Make Sure Backend Network exists
     network_create ${NET_BACKEND}
 
     ## Initialize Docker Command
-    DOCKERCMD="docker run --rm"
+    local DOCKERCMD="docker run --rm"
 #    DOCKERCMD="docker run"
 
     # Set Server Configuration File
-    DOCKERCMD="${DOCKERCMD} -v ${CONF}:/app/server.json:ro"
+    DOCKERCMD="${DOCKERCMD} -v ${conf}:/app/server.json:ro"
 
     # Is Debug DB?
     if [ "$MODE" == "debug" ]; then
@@ -60,14 +49,189 @@ start_api() {
     fi
 
     # Add Image Name
-    DOCKERCMD="${DOCKERCMD} --name ${CONTAINER}"
-    DOCKERCMD="${DOCKERCMD} -d ${DOCKER_IMAGE}"
+    DOCKERCMD="${DOCKERCMD} --name ${container}"
+    DOCKERCMD="${DOCKERCMD} -d ${image}"
 
     # Execute the Command
     echo $DOCKERCMD
     $DOCKERCMD
 
     # Attach to Storage Backplane Network
-    connect_container ${NET_BACKEND} "${CONTAINER}"
+    connect_container ${NET_BACKEND} "${container}"
   fi
+}
+
+## Start Backend API Server
+api_start() {
+  # PARAM $1 - MODE
+
+  # Docker Image
+  local docker_image="local/${API_IMAGE}:${API_VERSION}"
+
+  # Action Execution State
+  echo "Working Mode [$1]"
+
+  # Make Sure Backend Network exists
+  network_create ${NET_BACKEND}
+
+  # Options based on Mode
+  case "$MODE" in
+    debug) # RabbitMQ: Debug Server
+      __api_start_server $docker_image "ov-api-debug" $1
+      ;;
+    single) # RabbitMQ: Single Server
+      __api_start_server $docker_image "ov-api-s1" $1
+      ;;
+    cluster) # RabbitMQ: Server Cluster
+      __api_start_server $docker_image "ov-api-c1" $1
+      __api_start_server $docker_image "ov-api-c2" $1
+      ;;
+  esac
+}
+
+## Stops All API Servers (Depends on MODE)
+api_stop() {
+  # PARAM $1 - MODE
+
+  # Action Execution State
+  echo "Working Mode [$1]"
+
+  # Options based on Mode
+  case "$1" in
+    debug) # Debug DB Server
+      stop_container "ov-api-debug"
+      ;;
+    single) # NOT Debug: Single Shard Server
+      stop_container "ov-api-s1"
+      ;;
+    cluster) # NOT Debug: Dual Shard Server
+      stop_container "ov-api-c1" &
+      stop_container "ov-api-c2" &
+      ;;
+  esac
+}
+
+## Attach Logger to API Server Container
+api_log() {
+  # PARAM $1 - MODE
+
+  # Action Execution State
+  echo "Working Mode [$1]"
+
+  # Options based on Mode
+  case "$MODE" in
+    debug) # RabbitMQ: Debug Server
+      logs_container "ov-api-debug"
+      ;;
+    single) # RabbitMQ: Single Production Server
+      logs_container "ov-api-s1"
+      ;;
+    cluster) # RabbitMQ: Cluster
+      echo "Can't Log more than one server"
+      ;;
+  esac
+}
+
+## Attach Shell to API Server Container
+api_shell() {
+  # PARAM $1 - MODE
+
+  # Action Execution State
+  echo "Working Mode [$1]"
+
+  # Options based on Mode
+  case "$1" in
+    debug) # RabbitMQ: Debug Server
+      docker exec -it "ov-api-debug" /bin/ash
+      ;;
+    single) # RabbitMQ: Single Production Server
+      docker exec -it "ov-api-s1" /bin/ash
+      ;;
+    cluster) # RabbitMQ: Cluster
+      echo "Can't Attach to more than one server"
+      ;;
+  esac
+}
+
+## Build Docker Image for API Server
+api_build() {
+  # Docker Image
+  local docker_image="local/${API_IMAGE}:${API_VERSION}"
+
+  # Stage Image from GITHUB
+  github_clone_release "${API_REPO}" "${API_VERSION}"
+
+  # Build Docker Image
+  build_docker_image "${API_REPO}" "${docker_image}"
+}
+
+## Display Message Queue Help
+api_usage() {
+  # PARAM $1 - Main Executable Script
+  echo "Usage: $1 api [start|stop|log|shell|build] {debug|single|cluster}" >&2
+  echo "       $1 api [help] " >&2
+  echo >&2
+  echo "Action:"
+  echo "  start   - Start Container" >&2
+  echo "  stop    - Stop Container" >&2
+  echo "  log     - Display Docker logs for Container" >&2
+  echo "  shell   - Interactive shell for Container" >&2
+  echo "  build   - Build Container Image" >&2
+  echo "  help    - Container usage message" >&2
+  echo >&2
+  echo "Possible MODES:"
+  echo "  debug   - Local Debug Model [DEFAULT]"
+  echo "  single  - Production: Single Server Mode"
+  echo "  cluster - Production: Clustered Server Mode"
+  echo >&2
+  echo "Examples:" >&2
+  echo >&2
+  echo "$1 api start       --- Start Container in [DEBUG] mode" >&2
+  echo "$1 api stop single --- Stop Container in  [SINGLE] mode" >&2
+  exit 3
+}
+
+## Execute Container Command
+api_command() {
+  # PARAM $1 - Main Executable Script
+  # PARAM $2 - Action
+  # PARAM $3, $4, $5 - per action parameters
+
+  # Action to Execute
+  case "$2" in
+    start)
+      # Start Container(s)
+      local mode=$(parameter_mode $3)
+      api_start ${mode}
+
+      ## List Running Containers
+      docker container ls
+      ;;
+    stop)
+      # Stop Container(s)
+      local mode=$(parameter_mode $3)
+      api_stop ${mode}
+
+      ## List Running Containers
+      docker container ls
+      ;;
+    log)
+      # Display Container Logs
+      local mode=$(parameter_mode $3)
+      api_log ${mode}
+      ;;
+    shell)
+      # Execute a Shell in a Container
+      local mode=$(parameter_mode $3)
+      api_shell ${mode}
+      ;;
+    build)
+      # Build Container Image
+      local mode=$(parameter_mode $3)
+      api_build ${mode}
+      ;;
+    *)
+      api_usage "$1"
+      ;;
+  esac
 }
--- lib/fe.sh
+++ lib/fe.sh
@@ -11,60 +11,224 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-## Build Docker Image for Frontend Web Server
-build_fe() {
-  REPO="frontend"
-  VERSION="v0.0.1"
-  IMAGE="ov-fe-server"
+# Frontend Server Properties
+FE_REPO="frontend"
+FE_VERSION="v0.0.1"
+FE_IMAGE="ov-fe-server"
 
-  # Stage Image from GITHUB
-  github_clone_release "${REPO}" "${VERSION}"
-
-  # Build Docker Image
-  build_docker_image "${REPO}" "local/${IMAGE}:${VERSION}"
-}
-
-## Start Frontend Web Server
-start_fe() {
-  # PARAM $1 - Container Name
-  CONTAINER=$1
-
-  # Docker Image
-  IMAGE="ov-fe-server"
-  VERSION="v0.0.1"
-  DOCKER_IMAGE="local/${IMAGE}:${VERSION}"
+## HELPERS ##
+__fe_start_server() {
+  # INPUTS
+  local image=$1     # Docker Image Name
+  local container=$2 # Container Name
+  local mode=$3      # Mode
 
   # Is Container Running?
-  status container "$CONTAINER"
+  status container "$container"
   if [[ $? == 0 ]]; then # NO
     ## Start Mongo
-    echo "Running container '$CONTAINER'"
+    echo "Running container '$container'"
 
     # Custom Configuration File
-    CONF="${CONTAINERDIR}/fe/config.${MODE}.js"
+    local conf="${CONTAINERDIR}/fe/config.${mode}.js"
 
     # Make Sure Backend Network exists
     network_create ${NET_BACKEND}
 
     ## Initialize Docker Command
-    DOCKERCMD="docker run --rm"
+    local DOCKERCMD="docker run --rm"
 #    DOCKERCMD="docker run"
 
     # Set Backend Configuration File
-    DOCKERCMD="${DOCKERCMD} -v ${CONF}:/usr/share/nginx/html/assets/config.js:ro"
+    DOCKERCMD="${DOCKERCMD} -v ${conf}:/usr/share/nginx/html/assets/config.js:ro"
 
     # Expose Port so that we can attach from local system
     DOCKERCMD="${DOCKERCMD} -p 127.0.0.1:5000:80"
 
     # Add Image Name
-    DOCKERCMD="${DOCKERCMD} --name ${CONTAINER}"
-    DOCKERCMD="${DOCKERCMD} -d ${DOCKER_IMAGE}"
+    DOCKERCMD="${DOCKERCMD} --name ${container}"
+    DOCKERCMD="${DOCKERCMD} -d ${image}"
 
     # Execute the Command
     echo $DOCKERCMD
     $DOCKERCMD
 
     # Attach to Storage Backplane Network
-    connect_container ${NET_BACKEND} "${CONTAINER}"
+    connect_container ${NET_BACKEND} "${container}"
   fi
+}
+
+## Start Frontend Server
+fe_start() {
+  # PARAM $1 - MODE
+
+  # Docker Image
+  local docker_image="local/${FE_IMAGE}:${FE_VERSION}"
+
+  # Action Execution State
+  echo "Working Mode [$1]"
+
+  # Make Sure Backend Network exists
+  network_create ${NET_BACKEND}
+
+  # Options based on Mode
+  case "$1" in
+    debug) # Debug Server
+      __fe_start_server $docker_image "ov-fe-debug" $1
+      ;;
+    single) # NOT Debug: Single Server
+      __fe_start_server $docker_image "ov-fe-s1" $1
+      ;;
+    cluster) # NOT Debug: Dual Server
+      __fe_start_server $docker_image "ov-fe-c1" $1
+      __fe_start_server $docker_image "ov-fe-c2" $1
+      ;;
+  esac
+}
+
+## Stops All Frontend Servers (Depends on MODE)
+fe_stop() {
+  # PARAM $1 - MODE
+
+  # Action Execution State
+  echo "Working Mode [$1]"
+
+  # Options based on Mode
+  case "$1" in
+    debug) # Debug Server
+      stop_container "ov-fe-debug"
+      ;;
+    single) # NOT Debug: Single Server
+      stop_container "ov-fe-s1"
+      ;;
+    cluster) # NOT Debug: Dual Server
+      stop_container "ov-fe-c1" &
+      stop_container "ov-fe-c2" &
+      ;;
+  esac
+}
+
+## Attach Logger to Frontend Server Container
+fe_log() {
+  # PARAM $1 - MODE
+
+  # Action Execution State
+  echo "Working Mode [$1]"
+
+  # Options based on Mode
+  case "$MODE" in
+    debug) # Debug Server
+      logs_container "ov-fe-debug"
+      ;;
+    single) # NOT Debug: Single Server
+      logs_container "ov-fe-s1"
+      ;;
+    cluster) # NOT Debug: Dual Server
+      echo "Can't Log more than one server"
+      ;;
+  esac
+}
+
+## Attach Shell to Frontend Server Container
+fe_shell() {
+  # PARAM $1 - MODE
+
+  # Action Execution State
+  echo "Working Mode [$1]"
+
+  # Options based on Mode
+  case "$1" in
+    debug) # Debug Server
+      docker exec -it "ov-fe-debug" /bin/ash
+      ;;
+    single) # NOT Debug: Single Server
+      docker exec -it "ov-fe-s1" /bin/ash
+      ;;
+    cluster) # NOT Debug: Dual Server
+      echo "Can't Attach to more than one server"
+      ;;
+  esac
+}
+
+## Build Docker Image for Frontend Server
+fe_build() {
+  # Docker Image
+  local docker_image="local/${FE_IMAGE}:${FE_VERSION}"
+
+  # Stage Image from GITHUB
+  github_clone_release "${FE_REPO}" "${FE_VERSION}"
+
+  # Build Docker Image
+  build_docker_image "${FE_REPO}" "${docker_image}"
+}
+
+## Display Frontend Server Help
+fe_usage() {
+  # PARAM $1 - Main Executable Script
+  echo "Usage: $1 fe [start|stop|log|shell|build] {debug|single|cluster}" >&2
+  echo "       $1 fe [help] " >&2
+  echo >&2
+  echo "Action:"
+  echo "  start   - Start Container" >&2
+  echo "  stop    - Stop Container" >&2
+  echo "  log     - Display Docker logs for Container" >&2
+  echo "  shell   - Interactive shell for Container" >&2
+  echo "  build   - Build Container Image" >&2
+  echo "  help    - Container usage message" >&2
+  echo >&2
+  echo "Possible MODES:"
+  echo "  debug   - Local Debug Model [DEFAULT]"
+  echo "  single  - Production: Single Server Mode"
+  echo "  cluster - Production: Clustered Server Mode"
+  echo >&2
+  echo "Examples:" >&2
+  echo >&2
+  echo "$1 fe start       --- Start Container in [DEBUG] mode" >&2
+  echo "$1 fe stop single --- Stop Container in  [SINGLE] mode" >&2
+  exit 3
+}
+
+## Execute Container Command
+fe_command() {
+  # PARAM $1 - Main Executable Script
+  # PARAM $2 - Action
+  # PARAM $3, $4, $5 - per action parameters
+
+  # Action to Execute
+  case "$2" in
+    start)
+      # Start Container(s)
+      local mode=$(parameter_mode $3)
+      fe_start ${mode}
+
+      ## List Running Containers
+      docker container ls
+      ;;
+    stop)
+      # Stop Container(s)
+      local mode=$(parameter_mode $3)
+      fe_stop ${mode}
+
+      ## List Running Containers
+      docker container ls
+      ;;
+    log)
+      # Display Container Logs
+      local mode=$(parameter_mode $3)
+      fe_log ${mode}
+      ;;
+    shell)
+      # Execute a Shell in a Container
+      local mode=$(parameter_mode $3)
+      fe_shell ${mode}
+      ;;
+    build)
+      # Build Container Image
+      local mode=$(parameter_mode $3)
+      fe_build ${mode}
+      ;;
+    *)
+      fe_usage "$1"
+      ;;
+  esac
 }
